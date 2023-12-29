63da8ddb013f69da0bef47d8d4955187bd20bb45
SebasYnojosa <emilio240104@gmail.com>
2023-12-28 22:07:53 -0400

    Compilar archivos SASS con tareas en el gulpfile

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,7 +1,22 @@
-function tarea(done) {
-    console.log("Mi primer tarea");
+const { src, dest, watch } = require("gulp");
+const sass = require('gulp-sass')(require('sass')); // Le deja a Gulp que busque los binarios de SASS para usarlos
+ 
+function css(done) {
+
+
+    src('src/scss/app.scss')            // Identifica archivo SASS
+        .pipe(sass())                   // Compila el archivo
+        .pipe(dest('build/css'));       // Almacena en el disco duro el archivo compilado
+
 
     done(); // Callback para anunciar que se ejecuto la funcion
 }
 
-exports.tarea = tarea;
+function dev(done) {
+    watch('src/scss/app.scss', css);    // Le asigna un watch a la funcion de compilar SASS
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
